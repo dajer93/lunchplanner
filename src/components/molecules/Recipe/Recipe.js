3cbb2dfee253559fecfcc341737855709844271a
dajer93 <varnai.martin@gmail.com>
2020-10-21 22:55:40 +0200

    Add delete button styles

--- src/components/molecules/Recipe/Recipe.js
+++ src/components/molecules/Recipe/Recipe.js
@@ -37,7 +37,7 @@ const Recipe = ({
       <Text type="h4">{name}</Text>
       {useDescription && <Text type="paragraph">{description}</Text>}
       {useIngredientsList && <List items={ingredients.map(item => ({ ...item, title: item.ingredient }))} />}
-      {useRemoveButton && <Button className="secondary sm" onClick={onRemove}>Remove</Button> }
+      {useRemoveButton && <Button className="delete sm" onClick={onRemove}>Remove</Button> }
     </div>
   );
 };
