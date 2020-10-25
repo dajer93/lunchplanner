cb080b86294b3043c323deb28446e76da539cfdf
dajer93 <varnai.martin@gmail.com>
2020-10-25 11:04:52 +0100

    Remove baseUrl; fix head

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: `http://localhost:8089`, // the url of our server
   headers: {
     "Content-Type": "application/json",
   },
